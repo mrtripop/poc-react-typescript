50c02cb0a4d922518f5e7deedccb5ca1b72493df
mrtripop <tripop.tor@qualint.io>
2022-10-13 01:48:02 +0700

    clean src folder

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+  title?: string
+  paragraph?: string
+}
+
+const Hero = (props: Props) => {
+  return (
+    <div style={{ backgroundColor: 'orange', padding: 10, margin: 10 }}>
+      <h1>{props.title}</h1>
+      <p>{props.paragraph}</p>
+    </div>
+  )
+}
+
+export default Hero
