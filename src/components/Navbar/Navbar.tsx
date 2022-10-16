542837fcd639a52cb675854cfa2a898e50f821be
mrtripop <tripop.tor@qualint.io>
2022-10-16 20:55:53 +0700

    learn navbar section

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
 import './Navbar.scss';
 import { images } from '../../constants';
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -17,6 +24,25 @@ const Navbar = (props: Props) => {
           </li>
         ))}
       </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                <li key={item} onClick={() => setToggle(false)}>
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
