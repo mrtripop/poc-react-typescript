bca659b967815839f08ee2e03e2c06c2cdc769f9
mrtripop <tripop.tor@qualint.io>
2022-11-05 19:55:25 +0700

    high order component HOC for share same function

---
+++ src/components/NavigationDots.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  active: string;
+};
+
+const NavigationDots = (props: Props) => {
+  return (
+    <div className='app__navigation'>
+      {['home', 'about', 'contact', 'work', 'skills', 'testimonials', 'contact'].map(
+        (item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className='app__navigation-dot'
+            style={props.active === item ? { backgroundColor: '#313BAC' } : {}}
+          />
+        ),
+      )}
+    </div>
+  );
+};
+
+export default NavigationDots;
---
+++ src/components/SocialMedia.tsx
@@ -0,0 +1,22 @@
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa';
+
+type Props = {};
+
+const SocialMedia = (props: Props) => {
+  return (
+    <div className='app__social'>
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <FaFacebook />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
